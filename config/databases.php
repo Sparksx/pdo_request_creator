b267603267094725052700a0a6302ecd56390c16
Sparks <hollisprice.iii@gmail.com>
2014-07-21 12:04:15 +0200

    Clean database config

--- config/databases.php
+++ config/databases.php
@@ -1,15 +1,6 @@
 <?php
-if(strpos($_SERVER['HTTP_HOST'], 'localhost') !== false || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false) {
-	define('db_host', 'localhost');
-	define('db_name', 'entre2courses');
+define('db_host', 'localhost');
+define('db_name', 'name of your database');
 
-	define('db_user', 'root');
-	define('db_pass', 'root');
-}
-else {
-	define('db_host', '127.0.0.1');
-	define('db_name', 'entre2courses');
-
-	define('db_user', 'root');
-	define('db_pass', 'cFngCUMBCQBi');
-}
+define('db_user', 'root');
+define('db_pass', '');
